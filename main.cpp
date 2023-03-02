337d5cf931fe13bd9bec7d4180b360b1be53616c
hweston <103822195+hweston@users.noreply.github.com>
2023-03-02 12:43:29 +0000

    Update main.cpp
    entanglement/superposition

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include <string>
 #include "Grid.h"
-#include "GetState.h"
+
 
 using namespace std;
 
@@ -8,18 +9,46 @@ using namespace std;
 int main()
 {
     string filename;
-    int n = 25;
+    int n = 6;
+    double B = 10;
     int steps;
+    int doe = 1;
+    string simulation;
+    string entangled = "entangled";
+    string standard = "standard";
+
+    cout << "Type of Simulation: ";
+    cin >> simulation;
+    if (simulation == entangled){
+        cout << "Enter number of entangled cells: ";
+        cin >> doe;
+    }
     cout << "Enter filename with initial grid: ";
     cin >> filename;
     cout << "Enter number of steps of simulation: ";
     cin >> steps;
-    Grid state = Grid(filename, n + 2);
 
-    for(int i =0;i<steps;i++){
-        state.GetNextState(n+2);
+    Grid state = Grid(filename, n + 2, B,simulation,doe);
+
+    if (simulation == standard) {
+
+                for (int i =0;i<steps;i++){
+                    state.GetNextState(n + 2, B, simulation,1);
+                }
+        }
+    if (simulation == entangled) {
+        for (int i =0;i<steps;i++){
+            state.GetNextState(n + 2, B,simulation, doe);
+        }
     }
-}
+
+
+    else {
+            cout << "Error" << endl;
+        }
+        }
+
+
 
 
 
