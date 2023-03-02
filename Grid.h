4ae6afa4c8c3636371d16209d113cf21168f4ee3
hweston <103822195+hweston@users.noreply.github.com>
2023-03-02 12:42:59 +0000

    Update Grid.h
    entanglement/superposition

--- Grid.h
+++ Grid.h
@@ -10,9 +10,11 @@ using namespace std;
 
 class Grid {
 public:
-    Grid(string filename, int n);
-    void GetNextState(int n);
-    vector<vector<float>> grid;
+    Grid(string filename, int n, double B, string simulation, int z);
+    void GetNextState(int n, double B, string simulation,int z);
+    vector<vector<vector<double>>> grid;
+    string standard = "standard";
+    string entangled = "entangled";
 
 private:
     int n;
