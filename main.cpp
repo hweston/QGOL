454fda42f08f71473f548e3a594273a660635432
hweston <103822195+hweston@users.noreply.github.com>
2023-05-04 18:18:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include "Grid.h"
+#include "Ent_Grid.h"
 #include <Eigen/Dense>
 
 
@@ -12,90 +13,92 @@ using namespace Eigen;
 
 int main() {
 
-    int n = 30;                     /* Gridsize */
-    double B = (pow(2,0.5)+1);                 /* Parameter controlling evolution rules */
-    int steps;                      /* Timesteps */
-    int z = 4;                      /* Size of grid vector 3rd dimension - corresponds to entangled states/alive/dead state */
-    int signal = 0;                 /* Signals wavefunction collapse - NB different for entangled vs. standard */
-    int cells;                      /* Stores total number of cells */
-    vector<int> collapsetimes(10);
-    int no_of_collapses;
+    int n = 8;                                     /* Gridsize */
+    double B = (pow(2, 0.5) + 1);     /* Parameter controlling evolution rules */
+    int steps;                                      /* Timesteps */
+    int states = 4;                                 /* Size of grid vector 3rd dimension - corresponds to entangled states/alive/dead state */
+    int cells;                                      /* Stores total number of cells */
+    vector<int> collapsetimes(10,10000);      /* Number of measurements */
+    int no_of_collapses;                            /* No. of collapses */
+    int signal = 0;                                 /* Signals measurement*/
 
     string filename;                /* Filename with initial seed */
     string coeff_file;              /* Filename with coefficients for each state */
     string simulation;              /* Specifies "entangled" or "standard simulation" */
-    string etype = "NA";            /* All entanglement or specified number of states */
     string coeff;                   /* Specifies whether coefficients are chosen or not */
-    string collapse;
-
-    for (int i = 0; i < collapsetimes.size(); i++) {
-        collapsetimes[i] = 10000;
-    }
+    string collapse;                /* Specifies whether any measurements take place */
 
     cout << "Type of Simulation: ";
     cin >> simulation;
+
+    cout << "Do you want the wavefunction to collapse? (y/n) ";
+    cin >> collapse;
+
+
     if (simulation == "standard") {
-        cout << "Do you want the wavefunction to collapse? (y/n) ";
-        cin >> collapse;
+
         if (collapse == "y") {
+
             cout << "How many times?: ";
             cin >> no_of_collapses;
+
             for (int i = 0; i < no_of_collapses; i++) {
+
                 cout << "Time of collapse " << i + 1 << ": ";
                 cin >> collapsetimes[i];
             }
 
         }
     }
+
     if (simulation == "entangled") {
-        cout << "All states(y) or a chosen number of states(n)?: ";
-        cin >> etype;
+
+        cout << "After which timestep?: ";
+        cin >> collapsetimes[0];
         cout << "Are coefficients for each state being chosen? (y/n): ";
         cin >> coeff;
+
         if (coeff == "y") {
             cout << "Please enter filename listing coeffient values: ";
             cin >> coeff_file;
         }
-        if (etype == "n") {
-            cout << "How many states are entangled?: ";
-            cin >> z;
-        } else {
 
-            cout << "Enter number of entangled states: ";
-            cin >> z;
-        }
-
-    }
-    vector<int> cellnum(z);   /* Stores number of cells in each entangled state */
-    if (etype == "n") {
+        cout << "How many states are entangled?: ";
+        cin >> states;
         cout << "How many total cells are entangled?: ";
         cin >> cells;
+    }
+
+    vector<int> cellnum(states);                 /* Stores number of cells in each entangled state */
+    if(simulation == "entangled"){
         int checkcells = 0;
-        for (int i = 0; i < z; i++) {
-            cout << "How many cells are in state " << i + 1
-                 << "?: ";
-            cin >> cellnum[i];
-            checkcells += cellnum[i];
-            if (i == z - 1) {
-                if (checkcells != cells) {
-                    cout
-                            << "Error: Your total cells doesn't match the sum of cells from each state. Please enter cell numbers again: ";
-                    i = 0;
+        for (int i = 0; i < states; i++) {
+                cout << "How many cells are in state " << i + 1
+                     << "?: ";
+                cin >> cellnum[i];
+                checkcells += cellnum[i];
+                if (i == states - 1) {
+                    if (checkcells != cells) {
+                        cout
+                                << "Error: Your total cells doesn't match the sum of cells from each state. Please enter cell numbers again: ";
+                        i = 0;
+                    }
                 }
             }
-        }
     }
+
 
     cout << "Enter filename with initial grid: ";
     cin >> filename;
     cout << "Enter number of steps of simulation: ";
     cin >> steps;
 
-
-    Grid state = Grid(filename, coeff_file, coeff, cells, cellnum, n + 2, simulation, etype, z);
-
     if (simulation == "standard") {
+
+        Grid state = Grid(filename, n + 2);
+
         for (int i = 0; i < steps; i++) {
+
             for (int a = 0; a < no_of_collapses; a++)
                 if (i == collapsetimes[a]) {
                     for (int j = 1; j < n + 1; j++) {
@@ -104,13 +107,17 @@ int main() {
                         }
                     }
                 }
-            state.GetNextState(n + 2, B, simulation, coeff, 1, etype, &signal);
+            state.GetNextState(n + 2, B);
         }
     }
-    if (simulation == "entangled") {
 
+    if (simulation == "entangled") {
+        Ent_Grid state = Ent_Grid(filename,coeff_file, coeff,cells,cellnum,n+2,states);
         for (int i = 0; i < steps; i++) {
-            state.GetNextState(n + 2, B, simulation, coeff, z, etype, &signal);
+            if (i == collapsetimes[0]) {
+                state.Ent_Collapse( n+2, coeff, &signal);
+            }
+            state.GetEntNextState(n + 2, B, coeff, states, &signal);
         }
     }
 
